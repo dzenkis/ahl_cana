f8344ab726bc4e7c4239f5304e8a50d3eab92c63
dzenkis <janohalagan@gmail.com>
2024-08-28 17:31:11 +0200

    cana v2.1 (overlay fixes)

--- public/assets/js/overlay.js
+++ public/assets/js/overlay.js
@@ -14,3 +14,11 @@ document.getElementById('openOverlay2').addEventListener('click', function() {
     document.getElementById('closeOverlay2').addEventListener('click', function() {
       document.getElementById('overlay2').classList.add('hidden');
     });
+
+    document.getElementById('openOverlay3').addEventListener('click', function() {
+      document.getElementById('overlay3').classList.remove('hidden');
+    });
+
+    document.getElementById('closeOverlay3').addEventListener('click', function() {
+      document.getElementById('overlay3').classList.add('hidden');
+    });
